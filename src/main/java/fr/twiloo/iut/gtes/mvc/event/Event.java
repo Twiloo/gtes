1a2f711e0bc4b87a5690024600d08a34993ab43b
Andrea Grasso <andrea.grasso@eolas.fr>
2024-12-17 18:57:02 +0100

    creation des events et test app

--- src/main/java/fr/twiloo/iut/gtes/mvc/event/Event.java
+++ src/main/java/fr/twiloo/iut/gtes/mvc/event/Event.java
@@ -4,6 +4,28 @@ public abstract class Event<P, R> {
     P payload;
     R response;
 
+    // Constructeur
+    public Event(P payload) {
+        this.payload = payload;
+    }
 
+    // Getters et Setteurs
+    public P getPayload() {
+        return payload;
+    }
 
+    public void setPayload(P payload) {
+        this.payload = payload;
+    }
+
+    public R getResponse() {
+        return response;
+    }
+
+    public void setResponse(R response) {
+        this.response = response;
+    }
+
+    // Méthode abstraite pour obtenir le nom de l'événement
+    public abstract String getName();
 }
---
+++ src/main/java/fr/twiloo/iut/gtes/mvc/event/EventBus.java
@@ -0,0 +1,24 @@
+package fr.twiloo.iut.gtes.mvc.event;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class EventBus {
+    private final List<Consumer<Event<?, ?>>> listeners = new ArrayList<>();
+
+    // Method to subscribe a listener
+    public void subscribe(Consumer<Event<?, ?>> listener) {
+        listeners.add(listener);
+    }
+
+    // Method to publish an event
+    public void publish(Event<?, ?> event) {
+        System.out.println("Event triggered: " + event.getName());
+        System.out.println("Payload: " + event.getPayload());
+
+        for (Consumer<Event<?, ?>> listener : listeners) {
+            listener.accept(event);
+        }
+    }
+}
